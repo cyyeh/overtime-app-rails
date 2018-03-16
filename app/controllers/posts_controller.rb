0a8c52d51b009f447e9dacce0507f01db82b9fb3
ChihYuYeh <cure100061148@gmail.com>
2018-03-16 22:16:23 +0800

    implement index view with posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show]
 
   def index
+    @posts = Post.all
   end
 
   def new
@@ -15,7 +16,7 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to @post, notice: 'Your post was created successfully'
     else
-      render new
+      render :new
     end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content")
-end
+@user = User.create(email: "chihyu.jimmy.yeh@gmail.com", password: "40423jimmy", password_confirmation: "40423jimmy", first_name: "ChihYu", last_name: "Yeh")
 
-User.create(email: "chihyu.jimmy.yeh@gmail.com", password: "40423jimmy", password_confirmation: "40423jimmy", first_name: "ChihYu", last_name: "Yeh")
+puts "1 user created"
+
+100.times do |post|
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
+end
 
 puts "100 posts have been created"
