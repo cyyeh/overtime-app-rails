e4b95167e9b72f35224dd0e588fcc9b0bef8467b
ChihYuYeh <cure100061148@gmail.com>
2018-03-18 12:00:42 +0800

    add post hout request

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ AdminUser.create(email: "root@gmail.com", password: "000000", password_confirmat
 puts "1 Admin User created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 posts have been created"
